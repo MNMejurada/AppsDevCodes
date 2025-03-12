44ec8c15dccab88d817146ce7ec121b797786e9f
Marco_Mejurada/OOPROG21 <150257388+MNMejurada@users.noreply.github.com>
2025-03-13 00:14:17 +0800

    Add files via upload

---
+++ MejuradaMidtermExam2/MejuradaMidtermExam2/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace MejuradaMidtermExam2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheckTriangle = new System.Windows.Forms.Button();
+            this.txtSide1 = new System.Windows.Forms.TextBox();
+            this.txtSide2 = new System.Windows.Forms.TextBox();
+            this.txtSide3 = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCheckTriangle
+            // 
+            this.btnCheckTriangle.Location = new System.Drawing.Point(271, 298);
+            this.btnCheckTriangle.Name = "btnCheckTriangle";
+            this.btnCheckTriangle.Size = new System.Drawing.Size(91, 27);
+            this.btnCheckTriangle.TabIndex = 0;
+            this.btnCheckTriangle.Text = "Enter";
+            this.btnCheckTriangle.UseVisualStyleBackColor = true;
+            this.btnCheckTriangle.Click += new System.EventHandler(this.btnCheckTriangle_Click);
+            // 
+            // txtSide1
+            // 
+            this.txtSide1.Location = new System.Drawing.Point(254, 110);
+            this.txtSide1.Name = "txtSide1";
+            this.txtSide1.Size = new System.Drawing.Size(131, 22);
+            this.txtSide1.TabIndex = 1;
+            // 
+            // txtSide2
+            // 
+            this.txtSide2.Location = new System.Drawing.Point(254, 165);
+            this.txtSide2.Name = "txtSide2";
+            this.txtSide2.Size = new System.Drawing.Size(131, 22);
+            this.txtSide2.TabIndex = 2;
+            // 
+            // txtSide3
+            // 
+            this.txtSide3.Location = new System.Drawing.Point(254, 226);
+            this.txtSide3.Name = "txtSide3";
+            this.txtSide3.Size = new System.Drawing.Size(131, 22);
+            this.txtSide3.TabIndex = 3;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(24, 45);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(45, 16);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Result";
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(121, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter Side #1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(121, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter Side #3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(121, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter Side #2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.Location = new System.Drawing.Point(94, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(319, 294);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox2.Controls.Add(this.lblResult);
+            this.groupBox2.Location = new System.Drawing.Point(160, 401);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(182, 103);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(506, 654);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSide3);
+            this.Controls.Add(this.txtSide2);
+            this.Controls.Add(this.txtSide1);
+            this.Controls.Add(this.btnCheckTriangle);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheckTriangle;
+        private System.Windows.Forms.TextBox txtSide1;
+        private System.Windows.Forms.TextBox txtSide2;
+        private System.Windows.Forms.TextBox txtSide3;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ MejuradaMidtermExam2/MejuradaMidtermExam2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MejuradaMidtermExam2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheckTriangle_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtSide1.Text, out double side1) || !double.TryParse(txtSide2.Text, out double side2) || !double.TryParse(txtSide3.Text, out double side3))
+            {
+                lblResult.Text = ("Invalid input. Please enter a valid number.");
+                return;
+            }
+
+            if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+            {
+                lblResult.Text = "Invalid Triangle";
+            }
+
+            if (side1 == side2 && side2 == side3)
+            {
+                lblResult.Text = "Equilateral Triangle";
+            }
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+            {
+                lblResult.Text = "Isosceles Triangle";
+            }
+            else
+            {
+                lblResult.Text = "Scalene Triangle";
+            }
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
